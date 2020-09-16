b0160a1cf62fb898eca909e7638bccfc0aca38a3
Vipul Anil Devnani <vipuldevnani97@gmail.com>
2020-09-16 18:14:24 +0530

    Updates

---
+++ precache-manifest.80782d33b3ba4c65ac58c94afd83ed8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3d6dc8f3632edc2e9c14a9a1cc0aa3",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "96a955e94ffc7235070e",
+    "url": "/Monsters-Rolodex/static/css/main.70e84934.chunk.css"
+  },
+  {
+    "revision": "89412ccff4c48751ad1a",
+    "url": "/Monsters-Rolodex/static/js/2.0a4e41ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.0a4e41ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a955e94ffc7235070e",
+    "url": "/Monsters-Rolodex/static/js/main.0b777bfb.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monsters-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
